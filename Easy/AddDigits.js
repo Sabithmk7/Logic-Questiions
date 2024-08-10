d6c866d96a64b319967708e335ae64aecd637efb
Sabith Mk <115177227+Sabithx@users.noreply.github.com>
2024-08-10 09:57:23 +0530

    Create AddDigits.js

---
+++ Easy/AddDigits.js
@@ -0,0 +1,10 @@
+//Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+   return 1+(num-1)%9;
+};
